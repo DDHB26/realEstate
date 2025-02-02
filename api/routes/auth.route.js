914d21a07ca6b493a365ee65094b5836b92b543a
DDHB26 <baksishsinghhooda26@gmail.com>
2025-02-02 10:32:52 +0530

    signoutfunctionality backend

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { google, signin, signup } from '../controllers/auth.controller.js';
+import { google, signin, signup,signOut } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/sign-up',signup); // signup comes from the auth.controller.js
 router.post('/sign-in',signin); 
 router.post('/google',google);
-
+router.get('/signout',signOut); //as we are not sending any data to the server so we are using get method
 export default router; 
